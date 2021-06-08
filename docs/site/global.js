e2a5cc2cd34d2da5c4ac4dde61f5794dd0b1222f
Nathan Birch <birchn@byui.edu>
2021-06-08 07:01:37 -0600

    first commit

---
+++ docs/site/global.js
@@ -0,0 +1,60 @@
+// global constants
+var g = {};
+
+// course info
+g.courseCode = '';
+g.courseName = '   Level II';
+g.courseCodeName = g.courseCode + ' | ' + g.courseName;
+
+// lessons
+g.n01 = 'Lesson ' + '01';
+g.n02 = 'Lesson ' + '02';
+g.n03 = 'Lesson ' + '03';
+g.n04 = 'Lesson ' + '04';
+g.n05 = 'Lesson ' + '05';
+
+// module names
+g.u01 = g.n01 + ' - Diving Deeper Into JavaScript';
+g.u02 = g.n02 + ' - JavaScript Frameworks';
+g.u03 = g.n03 + ' - JavaScript Dependencies and Builds';
+g.u04 = g.n04 + ' - Testing and Offline Storage';
+g.u05 = g.n05 + ' - Asynchronous JavaScript';
+
+// assignment information
+
+g.reading = 'Reading Materials';
+g.classTime = 'In-class Materials';
+g.assignment = 'Programming Assignment';
+
+// course topics
+g.t1_0 = 'JavaScript Review';
+g.t1_1 = 'JavaScript Best Practices - Good Habits to get into';
+g.t1_2 = 'Helpful Programming Tips';
+g.t1_3 = 'JavaScript Modules';
+g.t1_4 = 'JavaScript Callbacks';
+g.t1_5 = 'Evolution of JavaScript';
+
+g.t2_1 = 'JavaScript Frameworks';
+g.t2_2 = 'Angular';
+g.t2_3 = 'React';
+g.t2_4 = 'Vue';
+g.t2_5 = 'Ember';
+g.t2_6 = 'Meteor';
+g.t2_7 = 'Node';
+g.t2_8 = 'Comparing and Deciding';
+g.t2_9 = 'Getting Started with our Framework of Choice';
+
+g.t3_1 = 'Single Page Applications';
+g.t3_2 = 'Dependencies in JavaScript';
+g.t3_3 = 'npm and npx';
+g.t3_4 = 'Builds';
+g.t3_5 = 'Variable Types, Repetition, Functions';
+g.t3_6 = 'ES6 Concepts';
+
+g.t4_1 = 'JavaScript Testing';
+g.t4_2 = 'Offline Storage';
+
+g.t5_1 = 'Asynchronous JavaScript';
+g.t5_2 = 'Recap on Starting a New Web App';
+g.t5_3 = 'Deploy App';
+g.t5_4 = 'Professional Portfolios';
