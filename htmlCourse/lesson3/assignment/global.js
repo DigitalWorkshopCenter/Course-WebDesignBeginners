a54ccfab02b249062e0585bf19a49482f056a4f0
= <birchn@byui.edu>
2021-08-11 09:43:44 -0600

    finished assignment 3 walkthrough and video

---
+++ htmlCourse/lesson3/assignment/global.js
@@ -0,0 +1,80 @@
+// global constants
+var g = {};
+
+// course info
+g.courseCode = '';
+g.courseName = '   Level II';
+g.courseCodeName = g.courseCode + ' | ' + g.courseName;
+
+// lessons
+g.n01 = 'Lesson ' + '01';
+g.n02 = 'Lesson ' + '02';
+g.n03 = 'Lesson ' + '03';
+g.n04 = 'Lesson ' + '04';
+g.n05 = 'Lesson ' + '05';
+g.n06 = 'Lesson ' + '06';
+
+// module names
+g.u01 = g.n01 + ' - Intro to Web Development (VS Code, HTML and CSS)';
+g.u02 = g.n02 + ' - Project I';
+g.u03 = g.n03 + ' - Advanced Web Development';
+g.u04 = g.n04 + ' - Project II';
+g.u05 = g.n05 + ' - Search Engine Optimization and Advanced CSS';
+g.u06 = g.n06 + ' - Project III';
+
+// assignment information
+
+g.reading = 'Reading Materials';
+g.classTime = 'In-class Materials';
+g.assignment = 'Programming Assignment';
+
+// course topics
+g.t1 = [
+  'Learn how to use VS Code (file structure, expansions, hotkeys, etc.)',
+  'Know what HTML and CSS are used for, respectively',
+  'Apply styles to your page',
+  'Create individual functional web pages using HTML5 and CSS3',
+  'Create a basic webpage layout',
+  'Create more web pages',
+  'Create lists',
+  'Structure content and use hyper-links',
+];
+
+g.t2 = [
+  'Learn how to use the browser inspect tools',
+  'Link CSS file',
+  'Lab Time: Project 1 -  Home Page',
+];
+
+g.t3 = [
+  'Design and develop an advanced webpage',
+  'Create intriguing, modern layouts using "div" and "span" tags',
+  'Use advanced CSS techniques and shortcuts (enhance text, format lists, modify background, background image properties, etc)',
+  'Incorporate Multimedia and Metadata',
+  'Work with tables the right way (style them with css)',
+  "Validate your web page's code",
+  'Create a breadcrumb',
+  'Review project architecture',
+];
+
+g.t4 = [
+  'Advanced CSS: create complex layouts, box layouts with rounded corners ',
+  'Advanced CSS: Print media queries',
+  'Incorporate accessibility features ',
+  'Lab Time: Project - Favorite Places',
+];
+
+g.t5 = [
+  'Optimize CSS for mobile - media queries',
+  'Create advanced styles and page layouts using CSS positioning',
+  'Create image and email links - format links with css',
+  'Learn the latest CSS3 features including shadows, gradients',
+  'Optimize a website for accessibility',
+  'Optimize a website for search engines - discuss site maps',
+  'Create basic forms (style with css, include advanced elements and form submission)',
+];
+
+g.t6 = [
+  'Website project: Personal Project',
+  'Lab Time: Publish a website - file upload: https://www.000webhost.com/',
+];
