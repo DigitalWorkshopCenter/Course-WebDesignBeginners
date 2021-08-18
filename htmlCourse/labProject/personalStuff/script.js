535198d44334486071473eca23271291ce8fa66b
= <birchn@byui.edu>
2021-08-18 10:37:59 -0600

    Finished up Lab Project Part 2

---
+++ htmlCourse/labProject/personalStuff/script.js
@@ -0,0 +1,31 @@
+function yearDiffNow(d) {
+  let diff = new Date() - d;
+  return (diff / 1000 / 60 / 60 / 24 / 365).toFixed(1);
+}
+
+// run code when page loads
+window.onload = function () {
+  let boy = new Date(2021, 12, 1).getTime();
+  document.getElementById('boy').innerHTML = yearDiffNow(boy);
+
+  let william = new Date(2019, 9, 1).getTime();
+  document.getElementById('william').innerHTML = yearDiffNow(william);
+
+  let clara = new Date(2018, 4, 1).getTime();
+  document.getElementById('clara').innerHTML = yearDiffNow(clara);
+
+  let emily = new Date(2016, 11, 1).getTime();
+  document.getElementById('emily').innerHTML = yearDiffNow(emily);
+
+  let hannah = new Date(2014, 12, 1).getTime();
+  document.getElementById('hannah').innerHTML = yearDiffNow(hannah);
+
+  let nathan = new Date(1992, 7, 1).getTime();
+  document.getElementById('nathan').innerHTML = yearDiffNow(nathan);
+
+  let bella = new Date(2021, 4, 1).getTime();
+  document.getElementById('bella').innerHTML = yearDiffNow(bella);
+
+  let daisy = new Date(2021, 5, 1).getTime();
+  document.getElementById('daisy').innerHTML = yearDiffNow(daisy);
+};
